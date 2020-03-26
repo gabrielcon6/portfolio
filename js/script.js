c5bcb45c3240042195decba9ec213c7e33e972cc
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-03-26 23:08:05 +1100

    fixed indentation

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 $(document).ready(function(){
-
-    $( ".cross" ).hide();
-    $( ".menu" ).hide();
-    $( ".hamburger" ).click(function() {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        $( ".menu" ).toggle( "slide", function() {
-        $( ".hamburger" ).hide();
-        $( ".cross" ).show();
-      });
-    });
-
-    $( ".cross, .menu-item" ).click(function() {
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".hamburger" ).click(function() {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
       $( ".menu" ).toggle( "slide", function() {
-        $( ".cross" ).hide();
-        $( ".hamburger" ).show();
-      });
-    });
-
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      } 
+      $( ".hamburger" ).hide();
+      $( ".cross" ).show();
     });
   });
+
+  $( ".cross, .menu-item" ).click(function() {
+    $( ".menu" ).toggle( "slide", function() {
+      $( ".cross" ).hide();
+      $( ".hamburger" ).show();
+    });
+  });
+
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
