1e2387b087d518df48df87c44d3b269a6abb34f1
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-03-26 19:54:25 +1100

    moved js file + fixed indentation

--- js/script.js
+++ js/script.js
@@ -3,33 +3,29 @@ $(document).ready(function(){
     $( ".cross" ).hide();
     $( ".menu" ).hide();
     $( ".hamburger" ).click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    $( ".menu" ).toggle( "slide", function() {
-    $( ".hamburger" ).hide();
-    $( ".cross" ).show();
-    });
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $( ".menu" ).toggle( "slide", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
+      });
     });
 
     $( ".cross, .menu-item" ).click(function() {
-    $( ".menu" ).toggle( "slide", function() {
-    $( ".cross" ).hide();
-    $( ".hamburger" ).show();
+      $( ".menu" ).toggle( "slide", function() {
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
+      });
     });
-    });
+
     $("a").on('click', function(event) {
-  
       if (this.hash !== "") {
         event.preventDefault();
-  
         var hash = this.hash;
-
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
-     
           window.location.hash = hash;
         });
       } 
     });
-    
   });
